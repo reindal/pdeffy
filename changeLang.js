7d3883811509e3e02fa5b617ae01081a5cf24b11
UKIRIWO <adriangogu1508@gmail.com>
2026-02-18 17:39:53 +0100

    MergeFixed

--- changeLang.js
+++ changeLang.js
@@ -168,7 +168,6 @@ const languages = {
         mustLeaveAtLeastOnePage: 'You must leave at least one page unselected.',
         processing: 'Processing...',
         successDeleted: 'Successfully created PDF: {filename}',
-        saveCancelled: 'Save cancelled',
         errorPrefix: 'Error: ',
 
         // PDF to DOCX
